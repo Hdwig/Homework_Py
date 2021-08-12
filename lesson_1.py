ed0139952d9c099f11c8113d48af29e5a92ccb55
Grigoriy <grigorianich@gmail.com>
2021-08-12 15:08:36 +0300

    Пурвое задание.

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+a = 13
+b = 12.5
+c = "Hello!"
+d = 5 > 3
+e = list[a, b, c, d]
+print(e)
+q = input("What is your number 1? ")
+w = input("What is your number 2? ")
+e = input("What is your name? ")
+r = input("What is your favorite fruit? ")
+print(f'"Hi, "{e} ')
+print(f'"Your 1 number is: "{q} ')
+print(f'"Your 2 number is:"{w} ')
+print(f'"your favorite fruit is:"{r} ')
